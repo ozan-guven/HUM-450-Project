68a64083f28bd51f5eef89e127420b20bb74990f
Arnaud Poletto <arnaud.poletto@epfl.ch>
2023-03-16 12:28:06 +0100

    refactor network js code

--- shs_website_test/js/networks.js
+++ shs_website_test/js/networks.js
@@ -1,6 +1,19 @@
 function load_network(
     container_selector, 
-    data_file
+    data_file,
+    exp_node_size_scale = 0.7,      // Exponent for scaling object sizes
+    exp_link_weight_scale = 0.7,    // Exponent for scaling link weights
+    transparency = 0.1,             // Transparency of non-selected nodes and links
+    min_node_size = 5,              // Minimum size of a node
+    max_node_size = 30,             // Maximum size of a node
+    min_link_weight = 1,            // Minimum weight of a link
+    max_link_weight = 8,            // Maximum weight of a link
+    label_size_add = 5,             // Additional size of label
+    node_size_inc = 5,              // Increment of node size when hovering
+    label_size_inc = 5,             // Increment of label size when hovering
+    charge_strength = -700,         // Strength of the charge force
+    collide_size_add = 5,           // Additional size of collision force
+    transition_duration = 200,      // Duration of transitions
     ) {
     d3.json(data_file).then(function(data) {
         // Define the container and get its size
@@ -8,20 +21,42 @@ function load_network(
         var width = container.node().getBoundingClientRect().width;
         var height = container.node().getBoundingClientRect().height;
 
-        var drag_active = false; // Drag behaviour, to skip mouseover events while dragging
-        var exp_size_scale = 0.7 // Exponent for scaling object sizes
-        var transparency = 0.1 // Transparency of non-selected nodes and links
-        var add_size = 7 // Add this to the size of the selected node
+        let drag_active = false; // Drag behaviour, to skip mouseover events while dragging
 
-        // Create the SVG
-        var svg = container.append("svg")
-            .attr("width", width)
-            .attr("height", height);
+        /**
+         * Map a node size to the given range
+         * @param {int} size 
+         * @returns 
+         */
+        function get_scaled_size(size, min_unscaled_size, max_unscaled_size, min_size, max_size) {
+            return min_size + (size - min_unscaled_size) * (max_size - min_size) / (max_unscaled_size - min_unscaled_size);
+        }
+
+        // Update each node's size to the scaled size
+        let max_unscaled_node_size = data.nodes.reduce((max, node) => Math.max(max, node.size), 0);
+        let min_unscaled_node_size = data.nodes.reduce((min, node) => Math.min(min, node.size), Infinity);
+        max_unscaled_node_size = Math.pow(max_unscaled_node_size, exp_node_size_scale);
+        min_unscaled_node_size = Math.pow(min_unscaled_node_size, exp_node_size_scale);
+
+        data.nodes.forEach(node => node.size = get_scaled_size(Math.pow(node.size, exp_node_size_scale), min_unscaled_node_size, max_unscaled_node_size, min_node_size, max_node_size));
+
+        // Update each link's weight to the scaled weight
+        let max_unscaled_link_weight = data.links.reduce((max, link) => Math.max(max, link.weight), 0);
+        let min_unscaled_link_weight = data.links.reduce((min, link) => Math.min(min, link.weight), Infinity);
+        max_unscaled_link_weight = Math.pow(max_unscaled_link_weight, exp_link_weight_scale);
+        min_unscaled_link_weight = Math.pow(min_unscaled_link_weight, exp_link_weight_scale);
+
+        data.links.forEach(link => link.weight = get_scaled_size(Math.pow(link.weight, exp_link_weight_scale), min_unscaled_link_weight, max_unscaled_link_weight, min_link_weight, max_link_weight));
 
         // Create linked list of nodes and isConnected function
         const linkedById = {};
         data.links.forEach(function(d) {
-            linkedById[d.source + "," + d.target] = 1;
+            linkedById[d.source] = []
+            linkedById[d.target] = []
+        });
+        data.links.forEach(function(d) {
+            linkedById[d.source].push(d.target);
+            linkedById[d.target].push(d.source);
         });
 
         /**
@@ -31,50 +66,58 @@ function load_network(
          * @returns {boolean} True if a and b are connected, false otherwise
          */
         function isConnected(a, b) {
-            return linkedById[a + "," + b] || linkedById[b + "," + a];
+            return linkedById[a].indexOf(b) > -1 || linkedById[b].indexOf(a) > -1 || a == b;
         }
+
+
 
         // Create the simulation
         var simulation = d3.forceSimulation()
             .force("link", d3.forceLink().id(function(d) { return d.id; }))
-            .force("charge", d3.forceManyBody().strength(-700))
+            .force("charge", d3.forceManyBody().strength(charge_strength))
             .force("center", d3.forceCenter(width / 2, height / 2))
-            .force("collide", d3.forceCollide().radius(function(d) { return 10 + Math.pow(d.size, exp_size_scale); }))
-            .on("tick", ticked);
+            .force("collide", d3.forceCollide().radius(function(d) { return d.size + collide_size_add; }));
 
+        // Create the SVG
+        let svg = container.append("svg")
+            .attr("width", width)
+            .attr("height", height);
+
+        // Create links
         var link = svg.append("g")
             .attr("stroke", "#BBB")
             .attr("stroke-opacity", 1.0)
             .selectAll("line")
             .data(data.links)
             .enter().append("line")
-            .attr("stroke-width", function(d) { return Math.pow(d.weight, exp_size_scale); });
+            .attr("stroke-width", function(d) { return d.weight; });
 
+        // Create nodes
         var gnodes = svg.selectAll("g.gnode")
             .data(data.nodes)
             .enter()
             .append("g")
             .classed("gnode", true);
 
-        // Visible node
+        // Create visible nodes
         var node = gnodes.append("circle")
             .attr("class", "node")
-            .attr("r", function(d) { return 3 + Math.pow(d.size, exp_size_scale); })
+            .attr("r", function(d) { return d.size; })
             .attr("fill", function(d) { return d.type == "vocation" ? "#FF3333" : "#29A329"; });
 
-        // Text label
+        // Create text labels
         var labels = gnodes.append("text")
             .text(function(d) { return d.id; })
-            .attr("font-size", function(d) { return 10 + Math.pow(d.size, Math.max(0.1, exp_size_scale - 0.1)); })
+            .attr("font-size", function(d) { return d.size + label_size_add; })
             .attr("font-family", "sans-serif")
             .attr("font-weight", "bold")
             .attr("text-anchor", "middle")
             .attr("alignment-baseline", "middle");
 
-        // Circle on top of text to make it easier to click
+        // Create circular zones around nodes to detect mouseover
         var clickCircle = gnodes.append("circle")
             .attr("class", "node")
-            .attr("r", function(d) { return 10 + Math.pow(d.size, exp_size_scale); })
+            .attr("r", function(d) { return d.size; })
             .attr("fill", "transparent")
             .call(drag(simulation))
             .on("mouseover", function(d) {
@@ -95,7 +138,7 @@ function load_network(
                         return isConnected(hoveredNodeId, o.id) || o.id == hoveredNodeId ? 1 : transparency;
                     })
                     .attr("r", function(o) {
-                        return isConnected(hoveredNodeId, o.id) || o.id == hoveredNodeId ? add_size + 3 + Math.pow(o.size, exp_size_scale) : 3 + Math.pow(o.size, exp_size_scale);
+                        return isConnected(hoveredNodeId, o.id) || o.id == hoveredNodeId ? o.size + node_size_inc : o.size;
                     });
 
                 // Not hovered link less opaque
@@ -114,7 +157,7 @@ function load_network(
                         return isConnected(hoveredNodeId, o.id) || o.id == hoveredNodeId ? 1 : transparency;
                     })
                     .attr("font-size", function(o) {
-                        return isConnected(hoveredNodeId, o.id) || o.id == hoveredNodeId ? add_size + 10 + Math.pow(o.size, Math.max(0.1, exp_size_scale - 0.1)) : 10 + Math.pow(o.size, Math.max(0.1, exp_size_scale - 0.1));
+                        return isConnected(hoveredNodeId, o.id) || o.id == hoveredNodeId ? o.size + label_size_add + label_size_inc : o.size + label_size_add;
                     });
             })
             .on("mouseout", function(d) {
@@ -124,18 +167,19 @@ function load_network(
 
                 // Reset opacity and sizes
                 node.transition()
-                    .duration(200)
+                    .duration(transition_duration)
                     .attr("fill-opacity", 1)
-                    .attr("r", function(d) { return 3 + Math.pow(d.size, exp_size_scale); });
+                    .attr("r", function(d) { return d.size; });
                 link.transition()
-                    .duration(200)
+                    .duration(transition_duration)
                     .attr("stroke-opacity", 1);
                 labels.transition()
-                    .duration(200)
+                    .duration(transition_duration)
                     .attr("fill-opacity", 1)
-                    .attr("font-size", function(d) { return 10 + Math.pow(d.size, Math.max(0.1, exp_size_scale - 0.1)); });
+                    .attr("font-size", function(d) { return d.size + label_size_add; });
             });
             
+        // Run simulation
         simulation
             .nodes(data.nodes)
             .on("tick", ticked);
@@ -143,6 +187,9 @@ function load_network(
         simulation.force("link")
             .links(data.links);
 
+        /**
+          * Update node and link positions at every step of the force simulation.
+          */
         function ticked() {
             link
                 .attr("x1", d => d.source.x)
@@ -162,9 +209,12 @@ function load_network(
                 .attr("x", d => d.x + 10)
                 .attr("y", d => d.y + 4)
                 .attr("dx", -9)
-                .attr("dy", "-0.15em");
+                .attr("dy", "-.15em");
         }
 
+        /**
+         * The drag simulation.
+         */
         function drag(simulation) {    
             function dragstarted(event) {
                 drag_active = true;
